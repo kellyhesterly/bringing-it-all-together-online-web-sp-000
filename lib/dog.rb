b2085d94867c9082bc9ac73a1a36e49ac8b39563
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-08 16:34:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -57,5 +57,9 @@ class Dog
   end
 
   def self.find_by_id(id)
+    sql = <<-SQL
+    SELECT * FROM dogs WHERE id = ? LIMIT 1
+    SQL
+    DB[:conn].execute(sql, id).map {|row| self.new_from_db(row)}.first
   end
 end
